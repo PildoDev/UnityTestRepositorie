590121765fb577b1c2784903db6879992ed014a9
Juan Felipe Ospina <61992581+PildoDev@users.noreply.github.com>
2023-05-12 11:25:41 -0500

    InicioScriptableObject
    Se inicio las configuraciones para el inventario

---
+++ My project/Assets/Scripts/Objects/ScriptObjectReferencia.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptObjectReferencia : MonoBehaviour
+{
+    public Sprite imagenObjeto;
+    public string nombreObjeto;
+    public string descripcionObjeto;
+    public int precioObjeto;
+}
